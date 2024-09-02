b8fbd5d1d32604c0b547b459388cdcb523c4918c
Maxi1972Tara <166968334+Maxi1972Tara@users.noreply.github.com>
2024-09-01 22:10:06 -0400

    Add files via upload

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage {
+export class LoginPage implements OnInit {
+  constructor(private router: Router) { }
+  email: string = '';
+  password: string = '';
+  passwordType: string = 'password';
+  ngOnInit() {
+  }
+  togglePasswordVisibility() {
+    this.passwordType = this.passwordType === 'password' ? 'text' : 'password';
+  }
+  validateEmail(email: string): boolean {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+  }
+  onLogin() {
+    console.log('Login button clicked');
+  }
+  onRegister() {
+    this.router.navigate(['/create-email']);
+  }
+  onRecoverPassword() {
+    this.router.navigate(['/recover-password']);
+  }
 }
